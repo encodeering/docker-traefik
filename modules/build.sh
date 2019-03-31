45acfbe1d6f8b9709bc5567174d17ebc79ac177e
Michael Clausen <encodeering@gmail.com>
2019-03-31 17:05:59 +0200

    updating scm repository to latest revision
    * efc10afc0bd0e538c2a6ef81801437f2b1b33c45

--- modules/build.sh
+++ modules/build.sh
@@ -5,8 +5,8 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.8" "alpine:3.8"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.9" "alpine:3.9"
 
 docker-build "$PROJECT/alpine"
 
-docker-verify version | dup | contains "v${VERSION}"
+docker-verify version | dup | matches "Version:[[:space:]]+${VERSION}"
